78e2dc078ad60bdaefd1cebaba7a062cc65b51f4
miaoruiyuan <miao201110@gmail.com>
2021-01-25 11:59:20 +0800

    init project

---
+++ Restaurant/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by miaoruiyuan on 2021/1/25.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by miaoruiyuan on 2021/1/25.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+
+@main
+struct RestaurantApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
